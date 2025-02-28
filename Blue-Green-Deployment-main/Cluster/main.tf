d1a893511860d1b84811b3b3c544ab8139d8622d
shilpahub348 <shilpasiddaram@gmail.com>
2025-02-28 17:15:55 +0530

    main.tf

--- Blue-Green-Deployment-main/Cluster/main.tf
+++ Blue-Green-Deployment-main/Cluster/main.tf
@@ -96,7 +96,7 @@ resource "aws_eks_cluster" "devopsshack" {
   }
 }
 
-resource "aws_eks_node_group" "devopsshack" {
+resource "aws_eks_node_group" "kubernetes" {
   cluster_name    = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
   node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
